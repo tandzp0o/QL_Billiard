92600d29ced71407698446f66161212134526753
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-10-27 21:44:51 +0700

    Update Ql_Billiard.sql

--- APP_QL_Billiard/SQL/Ql_Billiard.sql
+++ APP_QL_Billiard/SQL/Ql_Billiard.sql
@@ -133,12 +133,7 @@ BEGIN
       FROM dbo.DatTruoc
      WHERE ThoiGianToi <= @ThoiGianDatTruoc
 
-    SELECT @TrangThaiBan = TrangThai
-      FROM dbo.Ban
-     WHERE MaBan = @TrangThaiDatTruoc
-
     IF @TrangThaiDatTruoc = 0
-        AND @TrangThaiBan IS NULL
     BEGIN
         UPDATE dbo.Ban
            SET TrangThai = 2
@@ -399,3 +394,71 @@ go
 
 ---------------------------------------------------------------CỤC NÀY ALWAY NẰM DƯỚI CÙNG
 --Tạo job để thực thi stored procedure(tự động 1h 1 lần)
+USE [msdb]
+GO
+
+/****** Object:  Job [check và sửa trạng thái bàn đặt trước]    Script Date: 27/10/2023 20:47:09 ******/
+BEGIN TRANSACTION
+DECLARE @ReturnCode INT
+SELECT @ReturnCode = 0
+/****** Object:  JobCategory [[Uncategorized (Local)]]    Script Date: 27/10/2023 20:47:09 ******/
+IF NOT EXISTS (SELECT name FROM msdb.dbo.syscategories WHERE name=N'[Uncategorized (Local)]' AND category_class=1)
+BEGIN
+EXEC @ReturnCode = msdb.dbo.sp_add_category @class=N'JOB', @type=N'LOCAL', @name=N'[Uncategorized (Local)]'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+
+END
+
+DECLARE @jobId BINARY(16)
+EXEC @ReturnCode =  msdb.dbo.sp_add_job @job_name=N'check và sửa trạng thái bàn đặt trước', 
+		@enabled=1, 
+		@notify_level_eventlog=0, 
+		@notify_level_email=0, 
+		@notify_level_netsend=0, 
+		@notify_level_page=0, 
+		@delete_level=0, 
+		@description=N'No description available.', 
+		@category_name=N'[Uncategorized (Local)]', 
+		@owner_login_name=N'sa', @job_id = @jobId OUTPUT
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+/****** Object:  Step [s1: sử dụng poc [dbo].[TrangThaiBan]]    Script Date: 27/10/2023 20:47:10 ******/
+EXEC @ReturnCode = msdb.dbo.sp_add_jobstep @job_id=@jobId, @step_name=N's1: sử dụng poc [dbo].[TrangThaiBan]', 
+		@step_id=1, 
+		@cmdexec_success_code=0, 
+		@on_success_action=1, 
+		@on_success_step_id=0, 
+		@on_fail_action=2, 
+		@on_fail_step_id=0, 
+		@retry_attempts=0, 
+		@retry_interval=0, 
+		@os_run_priority=0, @subsystem=N'TSQL', 
+		@command=N'exec [dbo].[TrangThaiBan]', 
+		@database_name=N'Ql_Billiard', 
+		@flags=0
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_update_job @job_id = @jobId, @start_step_id = 1
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobschedule @job_id=@jobId, @name=N'do after 1 hour', 
+		@enabled=1, 
+		@freq_type=4, 
+		@freq_interval=1, 
+		@freq_subday_type=4, 
+		@freq_subday_interval=1, 
+		@freq_relative_interval=0, 
+		@freq_recurrence_factor=0, 
+		@active_start_date=20231027, 
+		@active_end_date=99991231, 
+		@active_start_time=0, 
+		@active_end_time=235959, 
+		@schedule_uid=N'8764f49a-8ea4-4320-9dfd-c70d0fea456e'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+EXEC @ReturnCode = msdb.dbo.sp_add_jobserver @job_id = @jobId, @server_name = N'(local)'
+IF (@@ERROR <> 0 OR @ReturnCode <> 0) GOTO QuitWithRollback
+COMMIT TRANSACTION
+GOTO EndSave
+QuitWithRollback:
+    IF (@@TRANCOUNT > 0) ROLLBACK TRANSACTION
+EndSave:
+GO
+
+----------------Không để dòng lệnh nào dưới này-------------------
