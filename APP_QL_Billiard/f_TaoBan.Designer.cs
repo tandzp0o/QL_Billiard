1d66bb855659bcfb00e1eda04aa7aa4d988802a5
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-10-30 12:22:34 +0700

    add f_TaoBan

---
+++ APP_QL_Billiard/f_TaoBan.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace APP_QL_Billiard
+{
+    partial class f_TaoBan
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.cbbLoaiBan = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.btnAdd);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.cbbLoaiBan);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(406, 240);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(50, 177);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(124, 48);
+            this.btnAdd.TabIndex = 4;
+            this.btnAdd.Text = "Thêm";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(134, 118);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(245, 34);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(11, 123);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 29);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Giá bàn";
+            // 
+            // cbbLoaiBan
+            // 
+            this.cbbLoaiBan.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbbLoaiBan.FormattingEnabled = true;
+            this.cbbLoaiBan.Location = new System.Drawing.Point(134, 53);
+            this.cbbLoaiBan.Name = "cbbLoaiBan";
+            this.cbbLoaiBan.Size = new System.Drawing.Size(245, 37);
+            this.cbbLoaiBan.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Loại bàn";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(104, 5);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(188, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Thêm bàn mới";
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(220, 177);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(124, 48);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            // 
+            // f_TaoBan
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(430, 264);
+            this.Controls.Add(this.panel1);
+            this.Name = "f_TaoBan";
+            this.Text = "Thêm bàn mới";
+            this.Load += new System.EventHandler(this.f_TaoBan_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbbLoaiBan;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
---
+++ APP_QL_Billiard/f_TaoBan.cs
@@ -0,0 +1,82 @@
+﻿using APP_QL_Billiard.DAO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace APP_QL_Billiard
+{
+    public partial class f_TaoBan : Form
+    {
+        public f_TaoBan()
+        {
+            InitializeComponent();
+        }
+
+        void loadCbbLoaiBan()
+        {
+            string sql = "Select distinct LoaiBan from Ban";
+            DataTable dt = DataProvider.Instance.ExcuteQuery(sql);
+            cbbLoaiBan.DataSource = dt;
+            cbbLoaiBan.DisplayMember = "LoaiBan";
+            cbbLoaiBan.ValueMember = "LoaiBan";
+        }
+        private void f_TaoBan_Load(object sender, EventArgs e)
+        {
+            loadCbbLoaiBan();
+
+        }
+
+        public string RemoveUnicode(string text)
+        {
+            string[] arr1 = new string[] { "á", "à", "ả", "ã", "ạ", "â", "ấ", "ầ", "ẩ", "ẫ", "ậ", "ă", "ắ", "ằ", "ẳ", "ẵ", "ặ", "đ", "é", "è", "ẻ", "ẽ", "ẹ", "ê", "ế", "ề", "ể", "ễ", "ệ", "í", "ì", "ỉ", "ĩ", "ị", "ó", "ò", "ỏ", "õ", "ọ", "ô", "ố", "ồ", "ổ", "ỗ", "ộ", "ơ", "ớ", "ờ", "ở", "ỡ", "ợ", "ú", "ù", "ủ", "ũ", "ụ", "ư", "ứ", "ừ", "ử", "ữ", "ự", "ý", "ỳ", "ỷ", "ỹ", "ỵ", "đ"};
+            string[] arr2 = new string[] { "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "a", "d", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "e", "i", "i", "i", "i", "i", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "o", "u", "u", "u", "u", "u", "u", "u", "u", "u", "u", "u", "y", "y", "y", "y", "y", "d"};
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                text = text.Replace(arr1[i], arr2[i]);
+                text = text.Replace(arr1[i].ToUpper(), arr2[i].ToUpper());
+            }
+            return text;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            string Ban = RemoveUnicode(cbbLoaiBan.SelectedValue.ToString());
+            string ma = DataProvider.Instance.ExcuteScalar<string>("Select top 1 MaBan from Ban where LoaiBan = N'" + cbbLoaiBan.SelectedValue.ToString() + "' order by MaBan desc");
+            string lastTwo = ma.Substring(ma.Length - 2);
+            int stt = int.Parse(lastTwo);
+            stt++;
+            if (stt < 10)
+            {
+                lastTwo = "0" + stt; 
+            }
+            else
+            {
+                lastTwo = stt.ToString();
+            }
+            string maban = Ban.Substring(0, 2) + lastTwo;
+            string tenban = "Bàn " + cbbLoaiBan.SelectedValue.ToString() + " " + stt;
+            string sql = "insert into Ban(MaBan, TenBan, LoaiBan, gia, trangthai) values ('" + maban + "', N'" + tenban + "', N'" + cbbLoaiBan.SelectedValue.ToString() + "', " + textBox1.Text + ", 2)";
+            int kq = DataProvider.Instance.ExcuteNonQuery(sql);
+            if (kq != 0)
+            {
+                MessageBox.Show("Thêm thành công", "Thông báo");
+            }
+            else
+            {
+                MessageBox.Show("Thêm không công", "Thông báo");
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+    }
+}
