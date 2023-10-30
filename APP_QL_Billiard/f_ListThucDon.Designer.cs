1d39585722e59a863024db666c8d16d9e84bcb11
tk5cdt <30490920+tk5cdt@users.noreply.github.com>
2023-10-31 00:23:03 +0700

    complete f_ListThucDon

--- APP_QL_Billiard/f_ListThucDon.Designer.cs
+++ APP_QL_Billiard/f_ListThucDon.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPicName = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtSL = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvThucDon = new System.Windows.Forms.DataGridView();
             this.btnAdd = new System.Windows.Forms.Button();
             this.txtPic = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
@@ -36,18 +42,27 @@
             this.label4 = new System.Windows.Forms.Label();
             this.cbbDVT = new System.Windows.Forms.ComboBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.txtPrice = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThucDon)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnEdit);
+            this.panel1.Controls.Add(this.btnReset);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.txtPicName);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.txtSL);
+            this.panel1.Controls.Add(this.label5);
+            this.panel1.Controls.Add(this.dgvThucDon);
             this.panel1.Controls.Add(this.btnAdd);
             this.panel1.Controls.Add(this.txtPic);
             this.panel1.Controls.Add(this.pictureBox1);
@@ -55,20 +70,84 @@
             this.panel1.Controls.Add(this.label4);
             this.panel1.Controls.Add(this.cbbDVT);
             this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.txtPrice);
             this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.txtName);
             this.panel1.Controls.Add(this.label1);
-            this.panel1.Controls.Add(this.dataGridView1);
             this.panel1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.panel1.Location = new System.Drawing.Point(12, 12);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(960, 494);
             this.panel1.TabIndex = 0;
+            this.panel1.Click += new System.EventHandler(this.panel1_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(393, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(85, 25);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Tên ảnh";
+            // 
+            // txtPicName
+            // 
+            this.txtPicName.Location = new System.Drawing.Point(503, 143);
+            this.txtPicName.Name = "txtPicName";
+            this.txtPicName.ReadOnly = true;
+            this.txtPicName.Size = new System.Drawing.Size(419, 30);
+            this.txtPicName.TabIndex = 16;
+            this.txtPicName.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(393, 188);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Đường dẫn";
+            // 
+            // txtSL
+            // 
+            this.txtSL.Location = new System.Drawing.Point(139, 143);
+            this.txtSL.Name = "txtSL";
+            this.txtSL.Size = new System.Drawing.Size(228, 30);
+            this.txtSL.TabIndex = 14;
+            this.txtSL.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrice_KeyPress);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(3, 148);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(90, 25);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "Số lượng";
+            // 
+            // dgvThucDon
+            // 
+            this.dgvThucDon.AllowUserToAddRows = false;
+            this.dgvThucDon.AllowUserToDeleteRows = false;
+            this.dgvThucDon.AllowUserToOrderColumns = true;
+            this.dgvThucDon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvThucDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvThucDon.Location = new System.Drawing.Point(3, 219);
+            this.dgvThucDon.MultiSelect = false;
+            this.dgvThucDon.Name = "dgvThucDon";
+            this.dgvThucDon.ReadOnly = true;
+            this.dgvThucDon.RowHeadersVisible = false;
+            this.dgvThucDon.RowHeadersWidth = 51;
+            this.dgvThucDon.RowTemplate.Height = 24;
+            this.dgvThucDon.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvThucDon.Size = new System.Drawing.Size(954, 272);
+            this.dgvThucDon.TabIndex = 12;
+            this.dgvThucDon.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThucDon_CellClick);
+            this.dgvThucDon.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvThucDon_CellContentClick);
             // 
             // btnAdd
             // 
-            this.btnAdd.Location = new System.Drawing.Point(142, 149);
+            this.btnAdd.Location = new System.Drawing.Point(8, 179);
             this.btnAdd.Name = "btnAdd";
             this.btnAdd.Size = new System.Drawing.Size(97, 34);
             this.btnAdd.TabIndex = 11;
@@ -78,14 +157,16 @@
             // 
             // txtPic
             // 
-            this.txtPic.Location = new System.Drawing.Point(503, 143);
+            this.txtPic.Location = new System.Drawing.Point(503, 183);
             this.txtPic.Name = "txtPic";
             this.txtPic.ReadOnly = true;
             this.txtPic.Size = new System.Drawing.Size(419, 30);
             this.txtPic.TabIndex = 10;
+            this.txtPic.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
             // 
             // pictureBox1
             // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
             this.pictureBox1.Location = new System.Drawing.Point(765, 27);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(157, 91);
@@ -111,15 +192,14 @@
             this.label4.Size = new System.Drawing.Size(90, 25);
             this.label4.TabIndex = 7;
             this.label4.Text = "Hình ảnh";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // cbbDVT
             // 
             this.cbbDVT.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cbbDVT.FormattingEnabled = true;
-            this.cbbDVT.Location = new System.Drawing.Point(142, 85);
+            this.cbbDVT.Location = new System.Drawing.Point(139, 85);
             this.cbbDVT.Name = "cbbDVT";
-            this.cbbDVT.Size = new System.Drawing.Size(225, 33);
+            this.cbbDVT.Size = new System.Drawing.Size(228, 33);
             this.cbbDVT.TabIndex = 6;
             // 
             // label3
@@ -131,12 +211,13 @@
             this.label3.TabIndex = 5;
             this.label3.Text = "Đơn vị tính";
             // 
-            // textBox2
+            // txtPrice
             // 
-            this.textBox2.Location = new System.Drawing.Point(503, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(225, 30);
-            this.textBox2.TabIndex = 4;
+            this.txtPrice.Location = new System.Drawing.Point(503, 27);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(225, 30);
+            this.txtPrice.TabIndex = 4;
+            this.txtPrice.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPrice_KeyPress);
             // 
             // label2
             // 
@@ -147,12 +228,12 @@
             this.label2.TabIndex = 3;
             this.label2.Text = "Giá bán";
             // 
-            // textBox1
+            // txtName
             // 
-            this.textBox1.Location = new System.Drawing.Point(142, 27);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(225, 30);
-            this.textBox1.TabIndex = 2;
+            this.txtName.Location = new System.Drawing.Point(139, 27);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(228, 30);
+            this.txtName.TabIndex = 2;
             // 
             // label1
             // 
@@ -163,15 +244,26 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "Tên mặt hàng";
             // 
-            // dataGridView1
+            // btnReset
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(3, 208);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(954, 283);
-            this.dataGridView1.TabIndex = 0;
+            this.btnReset.Location = new System.Drawing.Point(139, 179);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(97, 34);
+            this.btnReset.TabIndex = 18;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(270, 179);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(97, 34);
+            this.btnEdit.TabIndex = 19;
+            this.btnEdit.Text = "Sửa";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Visible = false;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
             // 
             // f_ListThucDon
             // 
@@ -181,10 +273,13 @@
             this.Controls.Add(this.panel1);
             this.Name = "f_ListThucDon";
             this.Text = "f_ListThucDon";
+            this.Load += new System.EventHandler(this.f_ListThucDon_Load);
+            this.Click += new System.EventHandler(this.f_ListThucDon_Click);
+            this.Leave += new System.EventHandler(this.f_ListThucDon_Leave);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvThucDon)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -192,10 +287,9 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox txtPrice;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox txtName;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ComboBox cbbDVT;
@@ -204,5 +298,13 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button btnPic;
         private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dgvThucDon;
+        private System.Windows.Forms.TextBox txtSL;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPicName;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnEdit;
     }
 }
